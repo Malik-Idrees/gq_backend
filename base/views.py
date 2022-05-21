f3bfb9ed638ec9f4716cd90838cffda3be8960c4
Malik Idrees <45917056+Malik-Idrees@users.noreply.github.com>
2022-05-21 16:35:51 +0500

    set main app name

--- base/views.py
+++ base/views.py
@@ -149,7 +149,7 @@ def profile(request):
         try:
             profile = Profile.objects.get(user=user)
             serializer = ProfileSerializer(profile,many=False)
-            return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
         except:
             return Response({'error':'profile does not exist'}, status=status.HTTP_400_BAD_REQUEST)
